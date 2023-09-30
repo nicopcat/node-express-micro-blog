5589635783c9b061c602117bcf8dc754af32f28b
Nic <tianfang07@outlook.com>
2023-09-30 18:14:00 +0800

    ????

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 require('dotenv').config()
-
+const path = require('path');
 const router = require('./routes/blogRoutes');
 
 const connectDB = require('./connectMongo')
 connectDB()
 // register view engine
 app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(morgan('tiny'));
 // middleware for static files
